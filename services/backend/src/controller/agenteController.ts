fdae8b2d39bcc4101f583f5c5bb25168c006594c
Livia Alves de Faria <livia.faria21@gmail.com>
2025-03-26 16:34:52 -0300

    ✨ feat: Adição do cadastro de agentes

---
+++ services/backend/src/controller/agenteController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { db } from '../app/app'; // Importa a conexão com o MySQL
+
+export default class AgenteController {
+
+    static async cadastrarAgente(agente: any) {
+        const { setor, assunto, documento } = agente;
+
+        const query = 'INSERT INTO agentes (setor, assunto, documento) VALUES (?, ?, ?)';
+        const values = [setor, assunto, documento];
+
+        return new Promise<any>((resolve, reject) => {
+            db.query(query, values, (err, results) => {
+                if (err) {
+                    console.error('Erro ao cadastrar agente:', err);
+                    reject({ success: false, message: 'Erro ao cadastrar agente', error: err });
+                } else {
+
+                    resolve({
+                        success: true,
+                        message: 'Agente cadastrado com sucesso',
+                    });
+                }
+            });
+        });
+    }
+}
--- services/backend/src/routes/route.ts
+++ services/backend/src/routes/route.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import multer, { StorageEngine } from 'multer';
+import { Request, Response } from 'express';
 import { db } from '../app/app'; // Importa a conexão com o MySQL
+import AgenteController from '../controller/agenteController';
 
 const router = Router();
 
@@ -8,18 +11,57 @@ router.post('/login', (req, res) => {
 
     // Consulta ao MySQL
     db.query("SELECT * FROM users WHERE email = ? AND password = ?", [email, senha], (err, results: any) => {
-      if (err) {
-          console.error("Erro no MySQL:", err); // <-- Adicione isso para ver o erro
-          return res.status(500).json({ error: "Erro ao consultar o banco de dados" });
-      }
-  
-      if (Array.isArray(results) && results.length > 0) {
-          res.status(200).json({ message: "Login bem-sucedido", user: results[0] });
-      } else {
-          res.status(401).json({ error: "Credenciais inválidas" });
-      }
-  });
-  
+        if (err) {
+            console.error("Erro no MySQL:", err); // <-- Adicione isso para ver o erro
+            return res.status(500).json({ error: "Erro ao consultar o banco de dados" });
+        }
+
+        if (Array.isArray(results) && results.length > 0) {
+            res.status(200).json({ message: "Login bem-sucedido", user: results[0] });
+        } else {
+            res.status(401).json({ error: "Credenciais inválidas" });
+        }
+    });
+
 });
+
+
+// configuração pro upload do arquivo pro cadastro do agente
+const storage = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage });
+
+// cadastro do agente
+router.post('/cadastro/agente', upload.single('documento'), async (req: Request, res: Response): Promise<void> => {
+    const agente = req.body;
+
+    if (!req.file) {
+        res.status(400).json({ success: false, message: "Documento não enviado." });
+        return;
+    }
+
+    agente.documento = req.file.path;
+
+    try {
+        const result = await AgenteController.cadastrarAgente(agente);
+
+        if (result.success) {
+            res.status(201).json(result);
+        } else {
+            res.status(500).json(result);
+        }
+    } catch (error) {
+        console.error('Erro ao cadastrar agente:', error);
+        res.status(500).json({ error: 'Erro ao cadastrar agente' });
+    }
+});
+
 
 export default router;
